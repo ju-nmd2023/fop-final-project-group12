01c4735ec56e9db2ef49331b8e83953d3721f93c
veronicasorge <150162661+veronicasorge@users.noreply.github.com>
2024-05-19 21:13:25 +0200

    Update levels.js

--- levels.js
+++ levels.js
@@ -278,7 +278,7 @@ function draw() {
     gameStart = false;
     gameIsOver = false;
     if (currentLevel === 1) {
-      drawLevel3();
+      drawLevel1();
     } else if (currentLevel === 2) {
       drawLevel2();
     } else if (currentLevel === 3) {
